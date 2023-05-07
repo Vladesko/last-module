2cdad4fa08f93655c248e7a1a570ecdb0331ccc4
Vladesko <borodich01@list.ru>
2023-05-07 22:54:47 +0300

    Initial

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Linq;
+
+namespace LastModule
+{
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Write your number");
+            string number = Console.ReadLine();
+            //Version 1.0
+            try
+            {
+                string json = JsonConvert.SerializeObject(number);
+                int value = JsonConvert.DeserializeObject<int>(json);
+                Console.WriteLine($"Your value is a {value}");
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Your number have a wrong format");
+            }
+        }
+    }
+}
